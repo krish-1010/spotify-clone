49092844f43689575a0c920ffebdccdd02695823
rithi <krishnamchan@gmail.com>
2023-07-06 08:40:05 +0530

    added like feature

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -12,12 +12,12 @@ interface ListItemProps {
 
 const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
   const router = useRouter();
-  const onClick = () => {
+  const onClick = (href: string) => {
     router.push(href);
   };
   return (
     <button
-      onClick={() => onClick}
+      onClick={() => onClick(href)}
       className="relative group flex items-center rounded-md overflow-hidden gap-x-4 bg-neutral-100/10 hover:bg-neutral-100/20 transition pr-4"
     >
       <div className="relative min-h-[64px] min-w-[64px]">
