105b51980d3390859e443792b0dc8d2290816462
krish-1010 <krishgithub10@gmail.com>
2025-01-21 05:16:23 +0530

    changed new supabase config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["wkbfbornaibtmdbqlupx.supabase.co"],
+    domains: ["hjdxmovflafuugltmbmz.supabase.co"],
   },
 };
 
